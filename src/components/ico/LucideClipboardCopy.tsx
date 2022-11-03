e670d4aa978de45b02c1fa70f9933381904ee87d
CodyAdam <cody.ad4@gmail.com>
2022-11-03 15:20:29 +0100

    feat: add icons

---
+++ src/components/ico/LucideClipboardCopy.tsx
@@ -0,0 +1,8 @@
+import React, { SVGProps } from 'react'
+
+export function LucideClipboardCopy(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}><g fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"><rect width="8" height="4" x="8" y="2" rx="1" ry="1"></rect><path d="M8 4H6a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-2M16 4h2a2 2 0 0 1 2 2v4m1 4H11"></path><path d="m15 10l-4 4l4 4"></path></g></svg>
+  )
+}
+export default LucideClipboardCopy
