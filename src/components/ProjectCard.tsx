ba9002d8101d178b39eb5462acab9a40e7369674
CodyAdam <cody.ad4@gmail.com>
2022-11-03 16:52:54 +0100

    feat: new interactive timeline

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -20,15 +20,17 @@ type Props = MarkdownInstance<Project>;
 export default function ProjectCard({ project }: { project: Props }) {
   return (
     <div className=''>
-      <div className='card max-w-xl bg-neutral shadow-xl'>
-        <figure>
-          <img src='https://placeimg.com/400/225/arch' alt='Shoes' className='object-cover w-full'/>
-        </figure>
+      <div className='card card-side max-w-4xl bg-neutral shadow-xl'>
+        {/* <figure>
+          <img src='https://placeimg.com/400/225/arch' alt='Shoes' className='object-cover h-full'/>
+        </figure> */}
         <div className='card-body'>
           <h2 className='card-title'>{project.frontmatter.title}</h2>
           <p>{project.frontmatter.sumary}</p>
-          <div className='card-actions justify-end'>
-            <a href={project.url} className='btn btn-primary'>Learn more</a>
+          <div className='card-actions justify-end pt-3'>
+            <a href={project.url} className='btn btn-primary'>
+              Learn more
+            </a>
           </div>
         </div>
       </div>
---
+++ src/components/Timeline.tsx
@@ -0,0 +1,165 @@
+import * as React from "react"
+import { SVGProps } from "react"
+
+const SvgComponent = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    id="Layer_2"
+    data-name="Layer 2"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    viewBox="0 0 310.34 636.57"
+    {...props}
+  >
+    <defs>
+      <style>
+        {
+          "\n      .cls-1 {\n        font-family: JetBrainsMonoNerdFontComplete-Bold, 'JetBrainsMono Nerd Font';\n      }\n\n      .cls-1, .cls-2, .cls-3, .cls-4, .cls-5, .cls-6, .cls-7 {\n        fill: #fff;\n      }\n\n      .cls-1, .cls-5 {\n        font-size: 24px;\n      }\n\n      .cls-8 {\n        fill: url(#radial-gradient-4);\n      }\n\n      .cls-3 {\n        font-size: 11px;\n      }\n\n      .cls-3, .cls-4, .cls-5, .cls-6, .cls-7 {\n        font-family: JetBrainsMonoNerdFontComplete-Regular, 'JetBrainsMono Nerd Font';\n      }\n\n      .cls-4 {\n        font-size: 16px;\n      }\n\n      .cls-9 {\n        fill: url(#radial-gradient);\n      }\n\n      .cls-10 {\n        font-family: JetBrainsMonoNerdFontComplete-Medium, 'JetBrainsMono Nerd Font';\n        font-size: 14px;\n      }\n\n      .cls-11 {\n        fill: url(#radial-gradient-2);\n      }\n\n      .cls-12 {\n        fill: #2f3742;\n      }\n\n      .cls-13 {\n        stroke: #2f3742;\n      }\n\n      .cls-13, .cls-14, .cls-15 {\n        fill: none;\n        stroke-linecap: round;\n        stroke-miterlimit: 10;\n        stroke-width: 2px;\n      }\n\n      .cls-16 {\n        fill: url(#radial-gradient-3);\n      }\n\n      .cls-14 {\n        stroke: url(#linear-gradient);\n      }\n\n      .cls-15 {\n        stroke: #fff;\n      }\n\n      .cls-6 {\n        font-size: 13px;\n      }\n\n      .cls-7 {\n        font-size: 10px;\n      }\n    "
+        }
+      </style>
+      <radialGradient
+        id="radial-gradient"
+        cx={99.66}
+        cy={601.84}
+        fx={99.66}
+        fy={601.84}
+        r={34.73}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset={0} stopColor="#e20aff" stopOpacity={0.3} />
+        <stop offset={0.42} stopColor="#8a069b" stopOpacity={0.18} />
+        <stop offset={1} stopColor="#000" stopOpacity={0} />
+      </radialGradient>
+      <radialGradient
+        id="radial-gradient-2"
+        cy={409.56}
+        fy={409.56}
+        r={41.97}
+        xlinkHref="#radial-gradient"
+      />
+      <radialGradient
+        id="radial-gradient-3"
+        cy={269.18}
+        fy={269.18}
+        r={54.88}
+        xlinkHref="#radial-gradient"
+      />
+      <radialGradient
+        id="radial-gradient-4"
+        cy={69.54}
+        fy={69.54}
+        r={69.54}
+        xlinkHref="#radial-gradient"
+      />
+      <linearGradient
+        id="linear-gradient"
+        x1={99.66}
+        y1={191.78}
+        x2={99.66}
+        y2={149.53}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset={0.44} stopColor="#fff" />
+        <stop offset={0.47} stopColor="#e1e2e3" />
+        <stop offset={0.54} stopColor="#94989e" />
+        <stop offset={0.62} stopColor="#2f3742" />
+      </linearGradient>
+    </defs>
+    <g id="Timeline">
+      <g>
+        <text className="cls-1" transform="translate(137.78 477.21)">
+          <tspan x={0} y={0}>
+            {"French "}
+          </tspan>
+          <tspan x={0} y={25}>
+            {"High "}
+          </tspan>
+          <tspan x={0} y={50}>
+            {"School "}
+          </tspan>
+          <tspan x={0} y={75}>
+            {"Diploma"}
+          </tspan>
+        </text>
+        <text className="cls-1" transform="translate(137.52 153.36)">
+          <tspan x={0} y={0}>
+            {"Master's "}
+          </tspan>
+          <tspan x={0} y={25}>
+            {"Degree in "}
+          </tspan>
+          <tspan x={0} y={50}>
+            {"Engineering"}
+          </tspan>
+        </text>
+        <text className="cls-1" transform="translate(136.77 320.26)">
+          <tspan x={0} y={0}>
+            {"University "}
+          </tspan>
+          <tspan x={0} y={25}>
+            {"Preparatory "}
+          </tspan>
+          <tspan x={0} y={50}>
+            {"Course"}
+          </tspan>
+        </text>
+        <g>
+          <circle className="cls-9" cx={99.66} cy={601.84} r={34.73} />
+          <text className="cls-7" transform="translate(87.66 604.65)">
+            <tspan x={0} y={0}>
+              {"2016"}
+            </tspan>
+          </text>
+          <circle className="cls-11" cx={99.66} cy={409.56} r={41.97} />
+          <text className="cls-6" transform="translate(84.06 412.38)">
+            <tspan x={0} y={0}>
+              {"2019"}
+            </tspan>
+          </text>
+          <circle className="cls-16" cx={99.66} cy={269.18} r={54.88} />
+          <text className="cls-4" transform="translate(80.46 272)">
+            <tspan x={0} y={0}>
+              {"2021"}
+            </tspan>
+          </text>
+          <circle className="cls-8" cx={99.66} cy={69.54} r={69.54} />
+          <text className="cls-5" transform="translate(70.86 62.87)">
+            <tspan x={0} y={0}>
+              {"2024"}
+            </tspan>
+          </text>
+          <text className="cls-3" transform="translate(66.66 78.51)">
+            <tspan x={0} y={0}>
+              {"Graduation"}
+            </tspan>
+          </text>
+          <path className="cls-15" d="M99.66,347.01v40.26" />
+          <circle className="cls-2" cx={99.66} cy={338.64} r={3.28} />
+          <path className="cls-15" d="M99.66,290.03v40.26" />
+          <path className="cls-15" d="M99.66,207.51v40.26" />
+          <circle className="cls-2" cx={99.66} cy={199.15} r={3.28} />
+          <path className="cls-14" d="M99.66,150.53v40.26" />
+          <circle className="cls-12" cx={99.66} cy={142.17} r={3.28} />
+          <path className="cls-13" d="M99.66,93.55v40.26" />
+          <path className="cls-15" d="M99.66,543.49v40.26" />
+          <circle className="cls-2" cx={99.66} cy={535.12} r={3.28} />
+          <path className="cls-15" d="M99.66,486.51v40.26" />
+          <circle className="cls-2" cx={99.66} cy={478.14} r={3.28} />
+          <path className="cls-15" d="M99.66,429.53v40.26" />
+          <g>
+            <path
+              className="cls-2"
+              d="M78,167.36l-7.2,9.6c-.24,.33-.56,.59-.93,.78-.38,.19-.78,.28-1.2,.28H2.67c-.73,0-1.36-.26-1.88-.78-.52-.52-.78-1.15-.78-1.88v-16c0-.73,.26-1.36,.78-1.88,.52-.52,1.15-.78,1.88-.78H68.67c.42,0,.82,.09,1.2,.28,.38,.19,.69,.45,.93,.78l7.2,9.6Z"
+            />
+            <text className="cls-10" transform="translate(6.77 171.22)">
+              <tspan x={0} y={0}>
+                {"present"}
+              </tspan>
+            </text>
+          </g>
+        </g>
+      </g>
+    </g>
+  </svg>
+)
+
+export default SvgComponent
