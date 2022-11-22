19f5ddba5bbf67d0b82a9631c99ea624436c6db5
CodyAdam <cody.ad4@gmail.com>
2022-11-22 16:31:53 +0100

    feat: animation duration longer width

--- src/components/Courses.tsx
+++ src/components/Courses.tsx
@@ -108,7 +108,7 @@ function Entry({ category, max }: { category: Category; max: number }) {
       <h1 className='w-52 text-xl font-semibold font-display text-left uppercase'>{category.title}</h1>
       <div className='flex items-center justify-between relative bg-black/30 grow w-full h-16 p-1 pr-3 rounded-lg  border border-neutral-focus  gap-4'>
         <div
-          className='h-full bg-gradient-to-tr from-red-600 to-yellow-300  rounded border border-white shadow-inner flex justify-center glow-shadow transition-all'
+          className='h-full bg-gradient-to-tr from-red-600 to-yellow-300  rounded border border-white shadow-inner flex justify-center glow-shadow transition-all duration-300'
           style={{ width: Math.round((category.hours / max) * 100) + '%' }}
         ></div>
         <div className='text-neutral-focus shrink-0'>
@@ -124,7 +124,7 @@ function SubEntry({ course, max }: { course: Course; max: number }) {
       <h1 className='w-40 text-sm font-medium text-left'>{course.title}</h1>
       <div className='flex items-center justify-between relative bg-black/30 grow w-full h-14 p-1 pr-3 rounded-lg  border border-neutral-focus gap-4'>
         <div
-          className='bg-gradient-to-tr from-slate-800 to-slate-500 h-full rounded border border-white/30 shadow-inner flex justify-center transition-all'
+          className='bg-gradient-to-tr from-slate-800 to-slate-500 h-full rounded border border-white/30 shadow-inner flex justify-center transition-all duration-300'
           style={{ width: Math.round((course.hours / max) * 100) + '%' }}
         ></div>
         <div className='text-neutral-focus shrink-0'>
